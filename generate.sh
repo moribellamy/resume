66269f71d78644114710033f114d846b9558d25f
Mori Bellamy <mori.michael.bellamy@gmail.com>
2018-02-06 14:53:39 -0800

    travis

--- generate.sh
+++ generate.sh
@@ -2,4 +2,4 @@
 
 OUTPUT=${1:-resume.html}
 
-node_modules/hackmyresume/dist/cli/index.js BUILD resume.json TO $OUTPUT -t node_modules/jsonresume-theme-elegant
+hackmyresume BUILD resume.json TO $OUTPUT -t node_modules/jsonresume-theme-elegant
