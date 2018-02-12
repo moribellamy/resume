437c6771a92aa1b5276fb3ccd769e80e26d20a39
Mori Bellamy <mori.michael.bellamy@gmail.com>
2018-02-12 15:07:22 -0800

    depend on 1.9.0 version of hackmyresume (@@@ issue fixed).
    Improve test script.

---
+++ test.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+./generate.sh resume-test.html
+if [[ $? -ne 0 ]]; then
+    echo "Could not run generation script."
+    exit 1
+fi
+
+diff resume.html resume-test.html
+if [[ $? -ne 0 ]]; then
+    echo "Checked in resume does not match resume.json."
+    exit 1
+fi
+
+grep -q @@@ resume-test.html
+if [[ $? -eq 0 ]]; then
+	echo "Test failed, '@@@' is not present in Mori's resume :)."
+	exit 1
+fi
