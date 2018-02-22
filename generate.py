5894b24e1496135703cf51be83ca77d2f671e434
Mori Bellamy <mori.michael.bellamy@gmail.com>
2018-02-21 22:09:39 -0800

    include PDFable resume as well

--- generate.py
+++ generate.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
+import os
 import subprocess
 import sys
 
 
-def main():
-    output = 'resume.html' if len(sys.argv) == 1 else sys.argv[1]
-
+# For web display. Inserts GA code.
+def write_modified_website(output):
     subprocess.check_call([
         'node_modules/hackmyresume/dist/cli/index.js',
         'BUILD',
@@ -26,5 +26,22 @@ def main():
         ofile.write(resume)
 
 
+# For PDF conversion. Must do manually with chrome.
+def write_html_for_pdf(output):
+    base, ext = os.path.splitext(output)
+    target = '%s-pdf%s' % (base, ext)
+    subprocess.check_call([
+        'node_modules/hackmyresume/dist/cli/index.js',
+        'BUILD',
+        'resume.json',
+        'TO',
+        target,
+        '-t',
+        'node_modules/jsonresume-theme-onepage'
+    ])
+
+
 if __name__ == '__main__':
-    main()
+    output = 'resume.html' if len(sys.argv) == 1 else sys.argv[1]
+    write_modified_website(output)
+    write_html_for_pdf(output)
--- test.sh
+++ test.sh
@@ -12,6 +12,12 @@ if [[ $? -ne 0 ]]; then
     exit 1
 fi
 
+diff resume-pdf.html resume-test-pdf.html
+if [[ $? -ne 0 ]]; then
+    echo "Checked in resume (PDF) does not match resume.json."
+    exit 1
+fi
+
 grep -q @@@ resume-test.html
 if [[ $? -eq 0 ]]; then
 	echo "Test failed, '@@@' is not present in Mori's resume :)."
